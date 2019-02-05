b754805f6a68bf0a8d96ec48a48c2ff57b925c7f
Anil Prakash D'souza  (BLR GSS) <anilprakashs@nousinfo.com>
2019-02-05 16:35:15 +0530

    Updates

---
+++ precache-manifest.c1e4335d79da0d92d74e7ff5b037be16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fb0b6d891eab8893df2",
+    "url": "/static/css/main.d00ccff2.chunk.css"
+  },
+  {
+    "revision": "9fb0b6d891eab8893df2",
+    "url": "/static/js/main.9fb0b6d8.chunk.js"
+  },
+  {
+    "revision": "53da9541540fed395cfa",
+    "url": "/static/css/1.e0940760.chunk.css"
+  },
+  {
+    "revision": "53da9541540fed395cfa",
+    "url": "/static/js/1.53da9541.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ca61d970b04deee327919b07336dcd4f",
+    "url": "/index.html"
+  }
+];
