6b33741a683b0d7f7cbbafc5717ac00367970a2d
Anil Prakash D'souza  (BLR GSS) <anilprakashs@nousinfo.com>
2019-03-04 13:14:44 +0530

    test

--- src/_components/Dashboard/UserInfo/index.js
+++
@@ -1,6 +0,0 @@
-export { default as UserInfo } from "./UserInfo";
-export { default as UserProfile } from "./Profile/UserProfile";
-export { default as ToDoInfo } from "./ToDoInfo/ToDoInfo";
-export { default as CurrentProject } from "./CurrentProject/CurrentProject";
-export { default as NewProject } from "./NewProject/NewProject";
-export { default as QuestionaireUpdates } from "./QuestionaireUpdates/QuestionaireUpdates";
